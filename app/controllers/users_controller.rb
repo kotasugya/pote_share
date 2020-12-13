997a41b3c2fbc91b0437d6eadb7650e641b97c02
kotasugya <no.shaking.throne974zero@gmail.com>
2020-12-13 16:59:08 +0900

    Create users_-new/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,40 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザー登録が完了しました"
+      riderect_to user_path(@user)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      flash[:success] = "登録情報を更新しました"
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
+  end
+
+  def destory
+  end
+
+  private
+   def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confrimation)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root 'home#top'
-  get "/login", to: 'sessions#new'
-  post "/login", to: 'sessions#create'
-  delete "/logout", to: 'sessions#destroy'
   resources :users
 end
